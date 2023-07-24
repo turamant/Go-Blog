98eae5f136edaed7bbc48cb4c178c8bcc97fdae7
askvart <tur1amant@gmail.com>
2023-07-24 23:41:34 +0300

    Insert()

--- pkg/models/mysql/posts.go
+++ pkg/models/mysql/posts.go
@@ -12,7 +12,17 @@ type PostModel struct {
 
 // This will insert a new snippet into the database.
 func (m *PostModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO posts (title, content, created, expires) 
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
