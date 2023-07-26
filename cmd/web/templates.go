3f3b8be989bdb3f5b1368d540a1b63ced64cad93
askvart <tur1amant@gmail.com>
2023-07-26 16:35:33 +0300

    cashing templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,39 @@
 package main
-import "github.com/turamant/Go-Blog/pkg/models"
 
+
+import (
+	"html/template"
+	"path/filepath"
+	"github.com/turamant/Go-Blog/pkg/models"
+)
 
 type templateData struct {
 	Post *models.Post
 	Posts []*models.Post
+}
+
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.html"))
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.html"))
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.html"))
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
 }
