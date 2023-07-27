0a0e970c9bdbe9885c058f94d894608406769511
askvart <tur1amant@gmail.com>
2023-07-27 19:13:57 +0300

    validate form signup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -86,8 +86,27 @@ func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	
+	form := forms.New(r.PostForm)
+	form.Required("name", "email", "password")
+	form.MaxLength("name", 255)
+	form.MaxLength("email", 255)
+	form.MatchesPattern("email", forms.EmailRX)
+	form.MinLength("password", 10)
+	
+	if !form.Valid() {
+		app.render(w, r, "signup.page.html", &templateData{Form: form})
+		return
+	}
+	
 	fmt.Fprintln(w, "Create a new user...")
-}
+}	
+
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display the user login form...")
