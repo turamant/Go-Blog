c57ebde148a1f33d92eaff0ab5dca729b0a60498
askvart <tur1amant@gmail.com>
2023-07-27 20:50:04 +0300

    dynamicMiddleware.Append(app.requireAuthenticated)

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,15 +15,15 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
-	mux.Get("/post/create", dynamicMiddleware.ThenFunc(app.createPostForm))
-	mux.Post("/post/create", dynamicMiddleware.ThenFunc(app.createPost))
+	mux.Get("/post/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createPostForm))
+	mux.Post("/post/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createPost))
 	mux.Get("/post/:id", dynamicMiddleware.ThenFunc(app.showPost))		
 	
 	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
 	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
-	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
