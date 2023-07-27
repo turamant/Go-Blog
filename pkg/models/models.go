89659d35494187794d1af55b798e1c7cc911681f
askvart <tur1amant@gmail.com>
2023-07-27 18:01:52 +0300

    model User

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,12 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var ( 
+	ErrNoRecord = errors.New("models: no matching record found")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
+
 
 type Post struct {
 	ID      int
@@ -13,4 +18,13 @@ type Post struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID int
+	Name string
+	Email string
+	HashedPassword []byte
+	Created time.Time
+	Active bool
 }
