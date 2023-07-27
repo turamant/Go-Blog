a7548c8db3cc0fdd1c47ed6250863390fdd7b42f
askvart <tur1amant@gmail.com>
2023-07-27 17:36:26 +0300

    /signup, /login,/logout  routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,12 @@ func (app *application) routes() http.Handler {
 	mux.Post("/post/create", dynamicMiddleware.ThenFunc(app.createPost))
 	mux.Get("/post/:id", dynamicMiddleware.ThenFunc(app.showPost))		
 	
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 	
